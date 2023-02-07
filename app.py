dacd65993c98e579b138d645bf18e887dc2c4a69
kXborg <kukilp213@gmail.com>
2023-02-07 18:27:08 +0530

    work update

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
 import folium
 import pandas as pd
 import streamlit as st
-from folium.plugins import HeatMap
+from folium.plugins import HeatMap, MarkerCluster
 from streamlit_folium import st_folium
-
-map = folium.Map(location=[51.521709, -0.212653], zoom_start=13)
-
-
-def plot_dot(point):
-    folium.CircleMarker(location=[point.Latitude, point.Longitude],
-                        radius=4,
-                        weight=2, 
-                        color='red',
-                        fill= True,
-                        fill_color='red').add_to(map)
-
 
 st.title('Mapping Potholes')
 
 ret = st.selectbox("Map Type", ('scatter', 'heatmap', 'satellite', 'street view'), index=0)
-print(ret)
-
+# print(ret)
+map = folium.Map(location=[51.521709, -0.212653], zoom_start=13)
 # Read potholes data from csv.
 df_acc = pd.read_csv('test-cv.csv', dtype=object)
+df_acc = df_acc.sample(20)
+
+clean_df = df_acc[['Latitude', 'Longitude']]
+clean_df = clean_df.dropna(axis=0, subset=['Latitude','Longitude'])
+clean_data = [[row['Latitude'],row['Longitude']] for _, row in clean_df.iterrows()]
 
 data = pd.DataFrame(df_acc, columns=['Latitude', 'Longitude'])
 # print(data[0:5])
@@ -32,19 +25,14 @@ df_acc['Latitude'] = df_acc['Latitude'].astype(float)
 df_acc['Longitude'] = df_acc['Longitude'].astype(float)
 
 if ret == 'heatmap':
-    heat_df = df_acc[['Latitude', 'Longitude']]
-    heat_df = heat_df.dropna(axis=0, subset=['Latitude','Longitude'])
-    heat_data = [[row['Latitude'],row['Longitude']] for index, row in heat_df.iterrows()]
-    HeatMap(heat_data).add_to(map)
+    HeatMap(clean_data).add_to(map)
 
 elif ret == 'scatter':
-    data.apply(plot_dot, axis=1)
-    # m.fit_bounds(m.get_bounds())
+    map = folium.Map(location=[51.521709, -0.212653], zoom_start=13)
+    MarkerCluster(clean_df).add_to(map)
+    
 
 elif ret == 'satellite':
-    map = folium.Map(location=[51.521709, -0.212653], zoom_start=13, tiles='Stamen Terrain')
-
-else:
-    pass
+    map = map
 
 st_data = st_folium(map, height=400, width=1920)
